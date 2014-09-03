b4ac465374908bb406be76c7955b6bd6de321e89
HugoMuller <hugo.muller@free.fr>
2014-09-03 15:44:51 +0200

    update logs

--- redis-twins.js
+++ redis-twins.js
@@ -2,25 +2,25 @@
 
 var redis = require('redis');
 
-var onError    = function(error){ console.log('Redis Regular Client ERROR: ', error); };
-var onSubError = function(error){ console.log('Redis Subscriber Client ERROR: ', error); };
-var onReady    = function(){ console.log('Redis Regular Client ready'); };
-var onSubReady = function(){ console.log('Redis Subscriber Client ready'); };
+var onError    = function(error){ console.log('Redis-twins Regular Client ERROR: ', error); };
+var onSubError = function(error){ console.log('Redis-twins Subscriber Client ERROR: ', error); };
+var onReady    = function(){ console.log('Redis-twins Regular Client ready'); };
+var onSubReady = function(){ console.log('Redis-twins Subscriber Client ready'); };
 
 var onSubscribe = function(channel, count){
-  console.log('Redis Client subscribing to channel', channel);
-  console.log('Redis Client has now', count, 'channel subscriptions');
+  console.log('Redis-twins subscribing to channel', channel);
+  console.log('Redis-twins has now', count, 'channel subscriptions');
 };
 
 var onUnsubscribe = function(channel, count){
-  console.log('Redis Subscriber Client unsubscribing from channel', channel);
-  console.log('Redis Subscriber Client has now', count, 'channel subscriptions');
+  console.log('Redis-twins unsubscribing from channel', channel);
+  console.log('Redis-twins has now', count, 'channel subscriptions');
   delete this.callbacks[channel];
 };
 
 var onMessage = function(channel, message){
   if(typeof this.callbacks[channel] === 'function') this.callbacks[channel](message);
-  else console.log('Redis Subscriber Client has no action associated to channel', channel);
+  else console.log('Redis-twins has no action associated to channel', channel);
 };
 
 var Rediscom = function(){
